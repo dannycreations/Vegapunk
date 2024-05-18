5ffa2da1b3e6d2e0ec3f5c611fcba59ab455e276
Danny Creations <usernamekepo@gmail.com>
2024-05-19 06:19:01 +0700

    update package.json, Vegapunk.ts and package.json

--- packages/core/src/lib/Vegapunk.ts
+++ packages/core/src/lib/Vegapunk.ts
@@ -1,8 +1,7 @@
 import { Store, StoreRegistry, container } from '@sapphire/pieces'
 import { Result } from '@sapphire/result'
-import { logger } from '@vegapunk/logger'
+import { Logger, logger } from '@vegapunk/logger'
 import { EventEmitter } from 'node:events'
-import type { Logger } from 'pino'
 import { ListenerStore } from './structures/ListenerStore'
 import { TaskStore } from './structures/TaskStore'
 
