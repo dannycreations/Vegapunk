3cb061ae93797190ee28a77c97de111db44ee709
dannycreations <44817214+dannycreations@users.noreply.github.com>
2024-07-06 12:53:46 +0700

    create 3 files, update 11 files, delete 4 files and copy 1 file

--- packages/core/src/lib/structures/Task.ts
+++ packages/core/src/lib/structures/Task.ts
@@ -1,7 +1,24 @@
 import { Piece } from '@sapphire/pieces'
-import { Result } from '@sapphire/result'
+import { TaskBase } from './TaskBase'
 
-export abstract class Task<Options extends Task.Options = Task.Options> extends Piece<Options, 'tasks'> {
+export abstract class Task<Options extends Task.Options = Task.Options> extends TaskBase<Options> {
+	public static createTask(initTask: Function, runTask: Function, options: Task.Options) {
+		if (typeof options.name === 'string') {
+			Object.assign(options, { name: options.name.toUpperCase() })
+		}
+
+		const uniq = `#${Date.now()}`
+		const context = { root: uniq, path: uniq, name: uniq, store: null }
+		const task = new TaskBase(context, options)
+
+		if (typeof initTask === 'function') task['runOnInit'] = initTask.bind(initTask)
+		if (typeof runTask === 'function') task['run'] = runTask.bind(runTask)
+		task.setDelay(options.delay)
+
+		task['_run'](true).then(() => task['_loop']())
+		return task
+	}
+
 	public constructor(context: Task.LoaderContext, options: Options = {} as Options) {
 		super(context, {
 			...options,
@@ -13,77 +30,6 @@ export abstract class Task<Options extends Task.Options = Task.Options> extends 
 
 	public runOnInit?(): unknown
 	public abstract run(...args: unknown[]): unknown
-
-	public get isStatus() {
-		return {
-			idle: this._isIdle,
-			stop: this._isStop,
-			running: this._isRunning,
-		}
-	}
-
-	public setDelay(delay: number) {
-		const maxDelay = 2147483647
-		if (delay > maxDelay) delay = maxDelay
-
-		this._delay = delay
-	}
-
-	public startTask(force?: boolean) {
-		this._isStop = false
-		this._loop(force)
-	}
-
-	public stopTask() {
-		this._isStop = true
-	}
-
-	private async _run(init?: boolean) {
-		this.container.logger.trace(`Task Run: ${this.options.name}`)
-		this._isRunning = true
-
-		const result = await Result.fromAsync(async () => {
-			if (!init) {
-				await this.run()
-			} else if (this.runOnInit) {
-				await this.runOnInit()
-			}
-		})
-		result.inspectErr((error) => this.container.logger.error(error, this.location.name))
-
-		this._isRunning = false
-		this.container.logger.trace(`Task End: ${this.options.name}`)
-	}
-
-	private get _status() {
-		if (this._isRunning) return true
-		if (this._isStop) {
-			delete this._timeout
-			this._isStop = false
-			return true
-		}
-	}
-
-	private async _loop(force?: boolean) {
-		clearTimeout(this._timeout)
-
-		if (this._status) return
-		if (force) await this._run()
-
-		this._isIdle = true
-		this._timeout = setTimeout(() => {
-			this._isIdle = false
-			if (this._status) return
-
-			this._run().then(() => this._loop())
-		}, this._delay)
-	}
-
-	private _delay: number = 0
-	private _isIdle: boolean = false
-	private _isStop: boolean = false
-	private _isRunning: boolean = false
-	private _timeout?: NodeJS.Timeout
 }
 
 export interface TaskOptions extends Piece.Options {
