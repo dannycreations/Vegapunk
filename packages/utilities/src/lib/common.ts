10ce68fa9dfe1b67ac323b5f417690903524ce5f
dannycreations <44817214+dannycreations@users.noreply.github.com>
2024-08-21 04:13:10 +0700

    update 6 files and create 3 files

--- packages/utilities/src/lib/common.ts
+++ packages/utilities/src/lib/common.ts
@@ -1,11 +1,9 @@
-import _chalk from 'chalk'
-import _humanizeDuration from 'humanize-duration'
+import chalk from 'chalk'
+import humanizeDuration from 'humanize-duration'
 import { type ParseError, type ParseOptions, parse } from 'jsonc-parser'
-import _lodash from 'lodash'
+import _ from 'lodash'
 
-export const chalk = _chalk
-export const lodash = _lodash
-export const humanizeDuration = _humanizeDuration
+export { _, chalk, humanizeDuration }
 
 export function parseJsonc<T>(text: string, errors?: ParseError[], options?: ParseOptions) {
 	return parse(text, errors, options) as T
