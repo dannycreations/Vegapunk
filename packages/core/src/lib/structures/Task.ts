e6ce2d181cc7dc94296bb7780b3eb3332e2bdeb0
dannycreations <44817214+dannycreations@users.noreply.github.com>
2024-09-08 19:19:38 +0700

    update 7 files

--- packages/core/src/lib/structures/Task.ts
+++ packages/core/src/lib/structures/Task.ts
@@ -1,23 +1,28 @@
 import { container, type Piece } from '@sapphire/pieces'
+import { VegapunkSnowflake } from '@vegapunk/utilities'
 import { HookPath } from './internal/StoreBase'
 import { TaskBase } from './internal/TaskBase'
 
 export abstract class Task<Options extends Task.Options = Task.Options> extends TaskBase<Options> {
-	public static createTask(task: CreateTask) {
+	public static async createTask(task: CreateTask) {
 		const _task = { ...task, options: { enabled: true, ...task.options } }
 
 		const taskStores = container.stores.get('tasks')
-		const context = { name: `${HookPath}${Date.now()}`, root: HookPath, path: HookPath, store: taskStores }
+		const uniq = VegapunkSnowflake.generate({ processId: BigInt(taskStores.size) })
+		const context = { name: `${HookPath}${uniq}`, root: HookPath, path: HookPath, store: taskStores }
 		const piece = new TaskBase(context, { ..._task.options, enabled: true }) as Task
+
+		const previous = taskStores.get(piece.name)
+		if (previous) await previous.unload()
 
 		if (typeof _task.awake === 'function') piece['awake'] = _task.awake.bind(_task.awake)
 		if (typeof _task.start === 'function') piece['start'] = _task.start.bind(_task.start)
 		if (typeof _task.update === 'function') piece['update'] = _task.update.bind(_task.update)
 		if (typeof _task.options.delay === 'number') piece.setDelay(_task.options.delay)
 		if (typeof _task.options.enabled === 'boolean') piece['_isEnable'] = _task.options.enabled
-		taskStores.set(piece.name, piece)
+		await taskStores.strategy.onLoad(taskStores, piece)
 
-		piece['_start'](true).then(() => piece['_update']())
+		taskStores.set(piece.name, piece)
 		return piece
 	}
 
--- packages/core/src/lib/structures/TaskLoaderStrategy.ts
+++ packages/core/src/lib/structures/TaskLoaderStrategy.ts
@@ -3,8 +3,9 @@ import { type Task } from './Task'
 import { type TaskStore } from './TaskStore'
 
 export class TaskLoaderStrategy extends LoaderStrategy<Task> {
-	public override onLoad(_store: TaskStore, piece: Task) {
-		piece['_start'](true).then(() => piece['_update']())
+	public override async onLoad(_store: TaskStore, piece: Task) {
+		await piece['_start'](true)
+		return piece['_update']()
 	}
 
 	public override onUnload(_store: TaskStore, piece: Task) {
