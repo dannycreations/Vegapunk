d2282abec4660b636cfc33a46a6f95997e9c1c20
dannycreations <44817214+dannycreations@users.noreply.github.com>
2024-09-22 16:00:32 +0700

    feat(utilities): update regex patterns for IPv4 and IPv6 support

--- packages/utilities/src/lib/regex.ts
+++ packages/utilities/src/lib/regex.ts
@@ -1,1 +1,4 @@
-export const IP_REGEX = /\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}/g
+// https://stackoverflow.com/a/17871737
+// https://gist.github.com/syzdek/6086792
+export const IPv4Regex = /(?:(?:25[0-5]|2[0-4]\d|1?\d\d?)\.){3}(?:25[0-5]|2[0-4]\d|1?\d\d?)/g
+export const IPv6Regex = /(?:[\da-f]{0,4}:){2,7}(?:(?<ipv4>(?:(?:25[0-5]|2[0-4]\d|1?\d\d?)\.){3}(?:25[0-5]|2[0-4]\d|1?\d\d?))|[\da-f]{0,4}|:)/gi
