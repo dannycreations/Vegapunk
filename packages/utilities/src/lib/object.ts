5db345c7ee428d437d70711030d55b84e67beb5a
dannycreations <44817214+dannycreations@users.noreply.github.com>
2024-10-16 07:00:48 +0700

    fix(utilities): prototype pollution issue

--- packages/utilities/src/lib/object.ts
+++ packages/utilities/src/lib/object.ts
@@ -1,17 +1,20 @@
+import { isObject } from 'es-toolkit/compat'
 import { DeepRequired, NestedKeyOf, NonNullObject, ValueAtPath } from './types'
 
-export function isObject(val: unknown): val is Record<string, unknown> {
-	return val !== null && typeof val === 'object' && !Array.isArray(val)
+function isSpecialProperty(key: string) {
+	return key === '__proto__' || key === 'constructor' || key === 'prototype'
 }
 
 export function defaultsDeep<A extends NonNullObject, B extends Partial<A> = Partial<A>>(target: A, ...sources: B[]) {
 	for (const source of sources) {
 		if (!isObject(source)) continue
 		for (const [key, sourceValue] of Object.entries(source)) {
+			if (isSpecialProperty(key)) continue
+
 			const targetValue = target[key as keyof A]
 			if (isObject(targetValue) && isObject(sourceValue)) {
-				defaultsDeep(targetValue as NonNullObject, sourceValue)
-			} else if (targetValue === undefined) {
+				defaultsDeep(targetValue, sourceValue)
+			} else if (typeof targetValue === 'undefined') {
 				target[key as keyof A] = sourceValue as A[keyof A]
 			}
 		}
@@ -27,9 +30,9 @@ export function strictGet<T extends NonNullObject, V extends NestedKeyOf<T>>(obj
 			result = (result as Record<string, unknown>)[key]
 		} else return value
 	}
-	return result === undefined ? value : (result as ValueAtPath<T, V>)
+	return typeof result === 'undefined' ? value : (result as ValueAtPath<T, V>)
 }
 
 export function strictHas<T extends NonNullObject>(obj: T, key: NestedKeyOf<T>) {
-	return strictGet(obj, key) !== undefined
+	return typeof strictGet(obj, key) !== 'undefined'
 }
