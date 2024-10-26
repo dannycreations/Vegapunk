d523779edd1a5474387f24e9cb012b952f16e83a
dannycreations <44817214+dannycreations@users.noreply.github.com>
2024-10-27 05:37:53 +0700

    feat(utilities): add new types

--- packages/utilities/src/lib/types.ts
+++ packages/utilities/src/lib/types.ts
@@ -1,9 +1,35 @@
 import { Sub } from './internal/types'
 
 // https://github.com/sapphiredev/utilities/blob/main/packages/utilities/src/lib/types.ts
+export type Nullish = null | undefined
+
+export type NonNullObject = {} & object
+
 export type Primitive = string | number | boolean | bigint | symbol | undefined | null
 
 export type Builtin = Primitive | Function | Date | Error | RegExp
+
+export type AbstractConstructor<T> = abstract new (...args: any[]) => T
+
+export type DeepReadonly<T> = T extends Builtin
+	? T
+	: T extends AbstractConstructor<unknown> | ((...args: any[]) => unknown)
+	? T
+	: T extends ReadonlyMap<infer K, infer V>
+	? ReadonlyMap<DeepReadonly<K>, DeepReadonly<V>>
+	: T extends ReadonlySet<infer U>
+	? ReadonlySet<DeepReadonly<U>>
+	: T extends readonly [] | readonly [...never[]]
+	? readonly []
+	: T extends readonly [infer U, ...infer V]
+	? readonly [DeepReadonly<U>, ...DeepReadonly<V>]
+	: T extends readonly [...infer U, infer V]
+	? readonly [...DeepReadonly<U>, DeepReadonly<V>]
+	: T extends ReadonlyArray<infer U>
+	? ReadonlyArray<DeepReadonly<U>>
+	: T extends object
+	? { readonly [K in keyof T]: DeepReadonly<T[K]> }
+	: unknown
 
 export type DeepRequired<T> = T extends Builtin
 	? NonNullable<T>
@@ -25,10 +51,6 @@ export type DeepRequired<T> = T extends Builtin
 	? { [K in keyof T]-?: DeepRequired<T[K]> }
 	: NonNullable<T>
 
-export type RequiredExcept<T, K extends keyof T> = Partial<Pick<T, K>> & Required<Omit<T, K>>
-
-export type PartialRequired<T, K extends keyof T> = Partial<Omit<T, K>> & Required<Pick<T, K>>
-
 export type DeepPartial<T> = {
 	[P in keyof T]?: T[P] extends Array<infer U>
 		? Array<DeepPartial<U>>
@@ -37,15 +59,23 @@ export type DeepPartial<T> = {
 		: DeepPartial<T[P]>
 }
 
-export type StrictRequired<T> = {
+export type RequiredExcept<T, K extends keyof T> = Partial<Pick<T, K>> & Required<Omit<T, K>>
+
+export type PartialExcept<T, K extends keyof T> = Partial<Omit<T, K>> & Required<Pick<T, K>>
+
+export type NonNullableObject<T> = {
+	[P in keyof T]: NonNullable<T[P]>
+}
+
+export type StrictObject<T> = {
 	[P in keyof T]-?: NonNullable<T[P]>
 }
 
+export type MutableObject<T> = {
+	-readonly [P in keyof T]: T[P] extends Array<unknown> | NonNullObject ? MutableObject<T[P]> : T[P]
+}
+
 export type Awaitable<T> = PromiseLike<T> | T
-
-export type Nullish = null | undefined
-
-export type NonNullObject = {} & object
 
 export type OnlyOneRequired<T, Key extends keyof T> = Pick<T, Exclude<keyof T, Key>> &
 	{ [K in Key]-?: Partial<Record<Exclude<Key, K>, never>> & Required<Pick<T, K>> }[Key]
