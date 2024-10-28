895f8ca0727a6136e88059cb0a2f1166d5d15d1d
dannycreations <44817214+dannycreations@users.noreply.github.com>
2024-10-28 17:01:21 +0700

    fix: bug fixes and improvements

--- packages/mikro-orm/tsup.config.ts
+++ packages/mikro-orm/tsup.config.ts
@@ -1,8 +1,3 @@
-import { esbuildPluginFilePathExtensions } from 'esbuild-plugin-file-path-extensions'
 import { createTsupConfig } from '../../scripts/tsup.config'
 
-export default createTsupConfig({
-	bundle: true,
-	entry: ['src/**/index.ts'],
-	esbuildPlugins: [esbuildPluginFilePathExtensions()],
-}) as unknown
+export default createTsupConfig({ entry: ['src/**/index.ts'] }) as unknown
