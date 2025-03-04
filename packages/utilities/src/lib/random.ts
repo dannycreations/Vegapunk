fe4dac3a97d39d42b1adaccdbbf1e8b69aeed4d8
dannycreations <44817214+dannycreations@users.noreply.github.com>
2025-03-05 04:31:05 +0700

    fix: bug fixes and improvements

--- packages/utilities/src/lib/random.ts
+++ packages/utilities/src/lib/random.ts
@@ -1,13 +1,13 @@
 import { customAlphabet } from 'nanoid'
 
-export enum Alphabet {
-	L = 'abcdefghijklmnopqrstuvwxyz',
-	U = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-	N = '0123456789',
-	S = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~',
+export const Alphabet = {
+	L: 'abcdefghijklmnopqrstuvwxyz',
+	U: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+	N: '0123456789',
+	S: '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~',
 }
 
-export function randomString(length = 30, ...args: Alphabet[]): string {
+export function randomString(length = 30, ...args: string[]): string {
 	args = args.length ? args : [Alphabet.L, Alphabet.U, Alphabet.N]
 	return customAlphabet(args.join(''), length)()
 }
