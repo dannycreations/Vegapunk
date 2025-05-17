9f6484620f3df4a7dfa2cd24c2f2194cc92fc78b
dannycreations <44817214+dannycreations@users.noreply.github.com>
2025-05-17 15:45:48 +0700

    fix(core): fix consistency

--- packages/core/scripts/task_seq.test.ts
+++ packages/core/scripts/task_seq.test.ts
@@ -29,104 +29,98 @@ console.log = (...args) => {
 }
 
 async function main(i = 0) {
-  setTimeout(Boolean, 60_000)
+  const client = new Vegapunk()
+  await client.start()
 
-  try {
-    const client = new Vegapunk()
-    await client.start()
+  const zeroTask = await Task.createTask({
+    awake: () => console.log(++i, '0.1 awake'),
+    start: () => console.log(++i, '0.2 start'),
+    update() {
+      console.log(++i, '0.3 update')
+      return zeroTask.unload()
+    },
+  })
 
-    const zeroTask = await Task.createTask({
-      awake: () => console.log(++i, '0.1 awake'),
-      start: () => console.log(++i, '0.2 start'),
-      update() {
-        console.log(++i, '0.3 update')
-        return zeroTask.unload()
-      },
-    })
+  const oneTask = await Task.createTask({
+    awake: () => console.log(++i, '1.1 awake'),
+    start: () => console.log(++i, '1.2 start'),
+    update() {
+      console.log(++i, '1.3 update')
+      return oneTask.unload()
+    },
+    options: { delay: 1100 },
+  })
 
-    const oneTask = await Task.createTask({
-      awake: () => console.log(++i, '1.1 awake'),
-      start: () => console.log(++i, '1.2 start'),
-      update() {
-        console.log(++i, '1.3 update')
-        return oneTask.unload()
-      },
-      options: { delay: 1100 },
-    })
+  const twoTask = await Task.createTask({
+    awake: () => console.log(++i, '2.1 awake'),
+    update() {
+      console.log(++i, '2.3 update')
+      return twoTask.unload()
+    },
+    options: { delay: 1200 },
+  })
 
-    const twoTask = await Task.createTask({
-      awake: () => console.log(++i, '2.1 awake'),
-      update() {
-        console.log(++i, '2.3 update')
-        return twoTask.unload()
-      },
-      options: { delay: 1200 },
-    })
+  const threeTask = await Task.createTask({
+    start: () => console.log(++i, '3.2 start'),
+    update() {
+      console.log(++i, '3.3 update')
+      return threeTask.unload()
+    },
+    options: { delay: 1300 },
+  })
 
-    const threeTask = await Task.createTask({
-      start: () => console.log(++i, '3.2 start'),
-      update() {
-        console.log(++i, '3.3 update')
-        return threeTask.unload()
-      },
-      options: { delay: 1300 },
-    })
+  const fourTask = await Task.createTask({
+    update() {
+      console.log(++i, '4.3 update')
+      return fourTask.unload()
+    },
+    options: { delay: 1400 },
+  })
 
-    const fourTask = await Task.createTask({
-      update() {
-        console.log(++i, '4.3 update')
-        return fourTask.unload()
-      },
-      options: { delay: 1400 },
-    })
+  const fiveTask = await Task.createTask({
+    awake: () => console.log(++i, '5.1 awake'),
+    start: () => console.log(++i, '5.2 start'),
+    update() {
+      console.log(++i, '5.3 update')
+      return fiveTask.unload()
+    },
+    options: { delay: 1500, enabled: false },
+  })
+  fiveTask.startTask()
 
-    const fiveTask = await Task.createTask({
-      awake: () => console.log(++i, '5.1 awake'),
-      start: () => console.log(++i, '5.2 start'),
-      update() {
-        console.log(++i, '5.3 update')
-        return fiveTask.unload()
-      },
-      options: { delay: 1500, enabled: false },
-    })
-    fiveTask.startTask()
+  const sixName = '6 test'
+  const sixTask = await Task.createTask({
+    update() {
+      console.log(++i, '6.3 update ' + sixTask.name)
+      return sixTask.unload()
+    },
+    options: { name: sixName, delay: 1600 },
+  })
 
-    const sixName = '6 test'
-    const sixTask = await Task.createTask({
-      update() {
-        console.log(++i, '6.3 update ' + sixTask.name)
-        return sixTask.unload()
-      },
-      options: { name: sixName, delay: 1600 },
-    })
+  const sevenTask = await Task.createTask({
+    awake: () => console.log(++i, '7.1 awake'),
+    start: () => console.log(++i, '7.2 start'),
+    update() {
+      console.log(++i, '7.3 update')
+      return sevenTask.unload()
+    },
+    options: { delay: 1700, enabled: false },
+  })
+  sevenTask.startTask(true)
 
-    const sevenTask = await Task.createTask({
-      awake: () => console.log(++i, '7.1 awake'),
-      start: () => console.log(++i, '7.2 start'),
-      update() {
-        console.log(++i, '7.3 update')
-        return sevenTask.unload()
-      },
-      options: { delay: 1700, enabled: false },
-    })
-    sevenTask.startTask(true)
+  const OBSERVER_TASK = await Task.createTask({
+    async update() {
+      if (i < expectedOutput.length) return
+      if (JSON.stringify(capturedLogs) === JSON.stringify(expectedOutput)) {
+        console.log('[v] Output matches the expected sequence!')
+      } else {
+        console.error('[x] Output does not match the expected sequence!')
+        console.error('Captured output:', capturedLogs)
+      }
 
-    const observerTask = await Task.createTask({
-      async update() {
-        if (i < expectedOutput.length) return
-        if (JSON.stringify(capturedLogs) === JSON.stringify(expectedOutput)) {
-          console.log('[v] Output matches the expected sequence!')
-        } else {
-          console.error('[x] Output does not match the expected sequence!')
-          console.error('Captured output:', capturedLogs)
-        }
-
-        await observerTask.unload()
-        process.exit(0)
-      },
-    })
-  } catch (error) {
-    console.error(error)
-  }
+      await OBSERVER_TASK.unload()
+    },
+    options: { ref: true },
+  })
 }
-main()
+main().catch(console.trace.bind(console))
