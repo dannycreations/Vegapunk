6e9867632701584520fe35507c836a1948b3256b
dannycreations <44817214+dannycreations@users.noreply.github.com>
2025-05-30 05:32:06 +0700

    fix(struct): bug fixes and improvements

--- packages/struct/src/lib/utils/common.ts
+++ packages/struct/src/lib/utils/common.ts
@@ -1,17 +1,26 @@
 /**
- * Swaps two elements in the given array (heap) at the specified indices.
+ * Swaps two elements at the specified indices within an array.
+ * This operation is performed in-place.
  *
  * @example
- * const arr = [1, 2, 3, 4];
- * swap(arr, 0, 3);
- * // arr is now [4, 2, 3, 1]
+ * const numbers = [10, 20, 30, 40];
+ * swap(numbers, 0, 3);
+ * console.log(numbers); // Output: [40, 20, 30, 10]
+ *
+ * try {
+ *   swap(numbers, 1, 5); // Attempting to swap with an out-of-bounds index
+ * } catch (e) {
+ *   if (e instanceof RangeError) {
+ *     console.error(e.message); // Output: Index out of bounds
+ *   }
+ * }
  *
  * @template T The type of elements in the array.
- * @param {T[]} heap The array in which elements will be swapped.
+ * @param {T[]} heap The array, often representing a heap, in which elements will be swapped.
  * @param {number} a The index of the first element to swap.
  * @param {number} b The index of the second element to swap.
  * @returns {void}
- * @throws {RangeError} If either index `a` or `b` is out of the bounds of the `heap` array.
+ * @throws {RangeError} If either index `a` or `b` is less than 0, or greater than or equal to the length of the `heap` array.
  */
 export function swap<T>(heap: T[], a: number, b: number): void {
   if (a < 0 || b < 0 || a >= heap.length || b >= heap.length) {
